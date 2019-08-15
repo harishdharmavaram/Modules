3dd415f58aa07d6d58fdc7f1f5227b5a8bb1d925
Harish Dharmavaram (CW) <Harish.Dharmavaram@jda.com>
2019-08-15 12:00:59 +0530

    changes

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -1,10 +1,8 @@
-provider "azurerm" {
-}
-terraform {
-  backend "azurerm" {
-    storage_account_name  = "jdateststrg"
-	resource_group_name   = "terraform-rg"
-    container_name        = "terragruntversion"
-    key                   = "terraform.tfstate"
+
+resource "azurerm_resource_group" "test" {
+  name    = "jtestgroup"
+  location  = "East us"
+ tags = {
+    environment = "testing"
   }
 }
--- resourcegroup/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "test" {
-  name    = "jtestgroup"
-  location  = "East us"
- tags = {
-    environment = "testing"
-  }
-}
