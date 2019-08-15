a72119c274d755fc1d9808fbe5ce304d5b695749
Harish Dharmavaram (CW) <Harish.Dharmavaram@jda.com>
2019-08-15 19:50:26 +0530

    copied

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -4,7 +4,7 @@ terraform {
   backend "azurerm" {
   storage_account_name  = "jdateststrg"
 	resource_group_name   = "terraform-rg"
-    container_name        = "terragruntversion"
+    container_name        = "versiontf"
     key                   = "terraform.tfstate"
   }
 }
